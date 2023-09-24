a44177d240935b296ffe2fb519c498ad3022641a
hassan <hassanvfx@gmail.com>
2023-09-23 21:33:28 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# S3DBaseGallery
+
+This framework was built with the ios-framework  config tool.
+[https://github.com/hassanvfx/ios-framework](https://github.com/hassanvfx/ios-framework)
---
+++ Sources/S3DBaseGallery/S3DBaseGallery.swift
@@ -0,0 +1,10 @@
+public struct S3DBaseGallery {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public func test(){
+        print("S3DBaseGalleryLib linked!")
+    }
+}
