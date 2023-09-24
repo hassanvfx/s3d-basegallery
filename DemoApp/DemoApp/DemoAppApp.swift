9ce7e633a08778153b662f300060a9e7d00ddc7f
hassan <hassanvfx@gmail.com>
2023-09-23 22:00:16 -0600

    ready

--- DemoApp/DemoApp/DemoAppApp.swift
+++ DemoApp/DemoApp/DemoAppApp.swift
@@ -6,21 +6,13 @@
 //
 
 import SwiftUI
-import S3DBaseGallery
 
-class Model:ObservableObject{
-    init(){
-        let framework = S3DBaseGallery()
-        framework.test()
-    }
-}
 
 @main
 struct DemoAppApp: App {
-    @ObservedObject var model = Model()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            GalleryView()
         }
     }
 }
